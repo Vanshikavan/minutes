dccab57f3e5afa8879c2abc4a7622244fdc073f0
VANSHIKA <vanshikakriika@gmail.com>
2026-01-28 15:28:46 +0530

    add gallery page

--- client/src/pages/Gallery.tsx
+++ client/src/pages/Gallery.tsx
@@ -1,13 +1,42 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import type { Project } from "../types"
+import {dummyGenerations} from "../../public/assets/assets"
+import { Loader2Icon } from "lucide-react"
 
 
 const Gallery = () => {
   const [projects, setProjects] = useState<Project[]>([])
   const [loading,setLoading] = useState(true)
-  return (
-    <div>
 
+  const fetchProjects = async ()=>{
+    setTimeout(()=>{
+      setProjects(dummyGenerations);
+      setLoading(false)
+    },3000)
+  }
+
+  useEffect(()=>{
+    fetchProjects()
+  },[])
+
+  return loading ? (
+    <div className="flex items-center justify-center min-h-screen">
+      <Loader2Icon className="size-7 animate-spin text-pink-400"/>
+    </div>
+  ) : (
+    <div className="min-h-screen text-white p-6 md:p-12 my-28">
+      <div className="max-w-6xl mx-auto">
+        <header className="mb-12">
+          <h1 className="text-3xl md:text-4xl font-semibold mb-4">Gallery</h1>
+          <p className="text-gray-400">See what others are creating with ADminutes</p>
+        </header>
+
+        <div className="columns-1 sm:columns-2 lg:columns-3 gap-4">
+          {projects.map((project)=>(
+            <div>{project.productName}</div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
